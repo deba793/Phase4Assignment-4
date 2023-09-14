e2ebfd52c492bef5f980af8cdf324c794843a806
deba793 <debasisrout793@gmail.com>
2023-09-14 14:50:46 +0530

    first commit

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Phase4Assignment_04.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private static List<Task> tasks = new List<Task>
+        {
+            new Task { Id = 1, Title = "Asp.net web api", Description = "Something starnge right!" },
+            new Task { Id = 2, Title = "Model view Controller", Description = "it is good" },
+             new Task { Id = 3, Title = "Console app .Net framework", Description = "It is easiest" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Task>> Get()
+        {
+            return tasks;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Task> Get(int id)
+        {
+            var task = tasks.Find(t => t.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return task;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Task task)
+        {
+            task.Id = tasks.Count + 1;
+            tasks.Add(task);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Task updatedTask)
+        {
+            var task = tasks.Find(t => t.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            task.Title = updatedTask.Title;
+            task.Description = updatedTask.Description;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var task = tasks.Find(t => t.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            tasks.Remove(task);
+            return NoContent();
+        }
+    }
+}
+    
+
---
+++ Task.cs
@@ -0,0 +1,9 @@
+﻿namespace Phase4Assignment_04
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
